70511952dc881c636a239b3cdb5b23f251f891f7
Maira Iantas <mairacristinaiantas@gmail.com>
2017-11-02 20:53:37 +0000

    Show screen for products created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'home#index'
+  root to: 'home#index'
+
+  resources :products, only: [:index, :show]
 end
