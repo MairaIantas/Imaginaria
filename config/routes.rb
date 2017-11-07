eae5b7142a99c815bf2f424d037b2db87bffdda1
Maira Iantas <mairacristinaiantas@gmail.com>
2017-11-07 00:10:01 +0000

    Created About Mode/Controller/Route and added to Active Admin control

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   get 'categories/show'
+  get '/about', to: 'about#about', as: 'about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106225925) do
+ActiveRecord::Schema.define(version: 20171107000603) do
+
+  create_table "abouts", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
