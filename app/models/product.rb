415679e581376e925a3535b53d27aaec0cb40b97
Maira Iantas <mairacristinaiantas@gmail.com>
2017-11-20 02:22:19 +0000

    3 products per page

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
     if term
       where('name LIKE ? ', "%#{term}").paginate(page: page, per_page: 2).order('id DESC')
     else
-      paginate(page: page, per_page: 2).order('id DESC')
+      paginate(page: page, per_page: 3).order('id DESC')
     end
   end
 end
