a0fdecbe6140aa89d53a2a2d903a2635653eb875
Maira Iantas <mairacristinaiantas@gmail.com>
2017-11-30 04:35:42 +0000

    Stripe payment

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,13 @@
 class CartsController < ApplicationController
   def show
     @order_item = current_order.items
+    @subtotal = get_subtotal
+  end
+
+  private
+  def get_subtotal
+    subtotal = 0
+      @order_item.each { |item| subtotal = subtotal + (item.qnty * item.price) }
+    subtotal
   end
 end
--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,21 +1,29 @@
 class ChargesController < ApplicationController
   def new
-    @amount = 500
+    @amount = get_subtotal
     @description = 'Description of Charge'
+    @order_item = current_order.items
   end
 
   def create
-    amount = 500 # $5 in cents
+     @amount = get_subtotal #get_subtotal # $5 in cents
 
-    @customer = Stripe::Customer.create(email:  params[:stripeEmail],
-                                       source: params[:stripeToken])
+    # @customer = Stripe::Customer.create(email:  params[:stripeEmail],
+                                       # source: params[:stripeToken])
 
-    @charge = Stripe::Charge.create(customer:    @customer.id,
-                                    amount:      amount,
+    @charge = Stripe::Charge.create(customer:    current_customer.id,
+                                    amount:      @amount,
                                     description: 'Rails Stripe customer',
                                     currency:    'cad')
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  private
+  def get_subtotal
+    (current_order.subtotal * 100)
+  end
+
 end
