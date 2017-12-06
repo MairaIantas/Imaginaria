5418618af0165ee61dfdf03df57ae90add8e7e0c
Maira Iantas <mairacristinaiantas@gmail.com>
2017-12-06 15:26:06 +0000

    last thing to do

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,14 +6,11 @@ class ChargesController < ApplicationController
 
   def create
     @amount = total
-
     @customer = Stripe::Customer.create(email: params[:stripeEmail], source: params[:stripeToken])
 
     @charge = Stripe::Charge.create(customer: @customer, amount: @amount, description: 'Imaginaria, thanks for buying with us! ',
                                     currency: 'cad')
-
-    @order = current_order
-    @order.update(status_id: 3)  
+    update_status
     session.delete(:order_id)
   rescue Stripe::CardError => e
     flash[:error] = e.message
@@ -24,5 +21,10 @@ class ChargesController < ApplicationController
 
   def total
     (current_order.total.to_i + 15) * 100
+  end
+
+  def update_status
+    @order = current_order
+    @order.update(status_id: 3)
   end
 end
